550d7c30b89dae839a2a4d22ade730b664b2c1c9
unknown <angelrubilar07@gmail.com>
2023-04-26 00:05:04 -0400

    pruebas con equipos, no van bien

---
+++ Austral-Equipos/js/botones.js
@@ -0,0 +1,11 @@
+
+function botones(){
+    document.querySelector('#botones').innerHTML +=
+    `<div class="mb-2" >    
+         <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 ">Listado</button>
+    </div>
+    <div>
+        <button type="button" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Agregar</button>
+    </div>
+    `;
+}
